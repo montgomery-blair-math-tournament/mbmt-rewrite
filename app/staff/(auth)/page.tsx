6ae2866cc0ff5280206f080668318d0d0de3edf2
Pen Guen <usual-blitz-bulge@duck.com>
2026-02-25 20:33:27 -0500

    color changes idk

--- app/staff/(auth)/page.tsx
+++ app/staff/(auth)/page.tsx
@@ -1,9 +1,19 @@
 import Heading from "@/components/Heading";
+import Button from "@/components/ui/Button";
 
 export default function StaffPage() {
     return (
         <div>
             <Heading level={1}>Staff Dashboard</Heading>
+            <p>Welcome!</p>
+            <div className="flex gap-4">
+                <Button variant={"default"}>default</Button>
+                <Button variant={"destructive"}>destructive</Button>
+                <Button variant={"outline"}>outline</Button>
+                <Button variant={"secondary"}>secondary</Button>
+                <Button variant={"ghost"}>ghost</Button>
+                <Button variant={"link"}>link</Button>
+            </div>
         </div>
     );
 }
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -4,18 +4,18 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 export const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+    "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none",
     {
         variants: {
             variant: {
                 default:
-                    "bg-rose-600 text-white hover:bg-rose-700 dark:bg-rose-700 dark:text-white dark:hover:bg-rose-800",
+                    "text-white bg-rose-600 hover:bg-rose-700 dark:bg-rose-700 dark:hover:bg-rose-800",
                 destructive:
-                    "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+                    "dark:text-white text-black bg-red-300 hover:bg-red-400 dark:bg-red-700 dark:hover:bg-red-800",
                 outline:
-                    "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+                    "border bg-white/0 hover:bg-gray-100 dark:hover:bg-gray-900",
                 secondary:
-                    "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+                    "bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800",
                 ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
                 link: "text-primary underline-offset-4 hover:underline",
             },
