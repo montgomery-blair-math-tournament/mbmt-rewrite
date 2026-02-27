c9f0e566f468327ae43b50d65b94040d134b7590
Pen Guen <usual-blitz-bulge@duck.com>
2026-02-26 23:56:34 -0500

    fetch teams

--- app/staff/(auth)/guts-grading/page.tsx
+++ app/staff/(auth)/guts-grading/page.tsx
@@ -1,13 +1,66 @@
 import Heading from "@/components/Heading";
-import GutsGradingForm from "./GutsGradingForm";
+import TeamsTable from "../teams/TeamsTable";
+import { useEffect, useState } from "react";
+import { TeamDisplay, TeamWithCount } from "@/lib/schema/team";
+import { createClient } from "@/lib/supabase/client";
+import { toast } from "sonner";
+import { DIVISIONS } from "@/lib/constants/settings";
 
 export default function GutsGradingPage() {
+    const [teams, setTeams] = useState<TeamDisplay[]>([]);
+    const [loading, setLoading] = useState(false);
+    const supabase = createClient();
+
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+            try {
+                const { data: teamsData, error } = await supabase
+                    .from("team")
+                    .select("*, participant!participant_team_id_fkey(count)");
+
+                if (error) {
+                    console.error("Error fetching teams:", error);
+                    setLoading(false);
+                    return;
+                }
+
+                const formattedData: TeamDisplay[] = (
+                    teamsData as TeamWithCount[]
+                ).map((t) => {
+                    const divisionCode = t.division ?? 0;
+                    const divisionInfo =
+                        DIVISIONS[divisionCode] || DIVISIONS[0];
+
+                    const displayId = `${divisionInfo.prefix}${t.id}`;
+
+                    return {
+                        id: t.id,
+                        displayId: displayId,
+                        name: t.name,
+                        school: t.school,
+                        chaperone: t.chaperone,
+                        division: divisionInfo.name,
+                        size: t.participant?.[0]?.count,
+                    };
+                });
+
+                setTeams(formattedData);
+            } catch (error) {
+                console.error(error);
+                toast.error("An error was encountered.");
+            } finally {
+                setLoading(false);
+            }
+        })();
+    });
+
     return (
         <div className="flex flex-col gap-6">
             <div className="flex justify-between items-center">
                 <Heading level={1}>Guts Grading</Heading>
 
-                <GutsGradingForm />
+                <TeamsTable teams={teams} loading={loading} />
             </div>
         </div>
     );
